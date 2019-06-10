2ae70593f79da11f8a9d5a3b76c68b15632bd7ee
lucascervera <lucas@lucascervera.com>
2019-06-10 17:12:32 +0200

    Source commit to github

---
+++ src/components/features.js
@@ -0,0 +1,77 @@
+import React from "react"
+
+export default () =>
+
+<div className="ui middle aligned stackable grid container">
+  <div className="row">
+    <div class='ui segments'>
+
+
+      <div className='ui segment'>Contracts are written as code</div>
+      <div className='ui segment'>Apply software development best practices to contract management</div>
+      <div className='ui segment'>Use existing software development tools</div>
+      <div className='ui segment'>Render contracts in multiple formats (pdf, markdown, html…)</div>
+
+
+
+      <div className='ui segment'>Integrate third-party services by calling APIs</div>
+      <div className='ui segment'>Define contract roles, users and access rights</div>
+
+      <div className='ui segment'>Apply version control best practices to contracts using Git</div>
+
+
+
+
+
+
+
+
+
+
+
+      <div className='ui segment'>Keep your contracts organized within an intuitive folder structure</div>
+
+      <div className='ui segment'>Availability and backup</div>
+      <div className='ui segment'>Synchronize all your contracts to a local folder in your computer</div>
+
+      <div className='ui segment'>View all your contracts in a detailed list with filters</div>
+
+      <div className='ui segment'>Master governance and easily prove compliance with your commitments</div>
+
+
+
+      <div className='ui segment'>Manage contract drafting and negotiation</div>
+      <div className='ui segment'>Create contracts by filling automatically generated forms</div>
+
+
+      <div className='ui segment'>Avoid starting from scratch by using contract templates</div>
+
+
+      <div className='ui segment'>Ensure contract compliance with applicable laws</div>
+
+
+
+
+
+      <div className='ui segment'>Easily view next possible actions in each contract</div>
+      <div className='ui segment'>Import relevant events to your google calendar</div>
+      <div className='ui segment'>Get reminders when an action is needed</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    </div>
+    </div>
